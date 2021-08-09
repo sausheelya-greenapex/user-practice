8f04e77300a9c8d64b26a31a972950ceb8a29e32
@sonali.p <sonali.p@pacewisdom.com>
2021-08-09 18:22:27 +0530

    first_commit

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+ User.create(first_name:"Vartika",last_name:"Tripathi",email:"Vartikaec@gamil.com")
+  User.create(first_name:"Sausheelya",last_name:"Tripathi",email:"sausheelya42@gmail.com")
+  User.create(first_name:"Krupali",last_name:"Patel",email:"krupali@greenapex.com")
+  User.create(first_name:"Jai",last_name:"Rajput",email:"jai@gmail.com")
